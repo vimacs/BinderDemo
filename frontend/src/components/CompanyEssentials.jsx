377d33fd6b4797918a5ac5cc7a9eb27b1220e51d
Vaibhav Anand <vaibhavkumaranand729@gmail.com>
2026-03-07 22:50:17 +0530

    Fix alias imports for utils

--- frontend/src/components/CompanyEssentials.jsx
+++ frontend/src/components/CompanyEssentials.jsx
@@ -5,7 +5,7 @@ import { Field } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { FormCard, FormRow, FullscreenContent } from '@/components/ui/form-layout';
-import { cn } from '@/lib/utils';
+import { cn } from '../lib/utils';
 import { getCompanyEssentials, createCompanyEssential } from '../services/integration';
 
 const CompanyEssentials = ({ onBack }) => {
